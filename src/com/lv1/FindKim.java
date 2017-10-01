9c221b754fc6d14a389ca7699ec19d3b6cf286d3
문주영 <enqlenwndud95@gmail.com>
2017-10-01 22:16:31 +0900

    김서방 찾기

--- src/com/lv1/FindKim.java
+++ src/com/lv1/FindKim.java
@@ -1,10 +1,19 @@
 package com.lv1;
 
 public class FindKim {
-
+	public String findKim(String[] seoul) {
+		int x = 0;
+		for(x=0; x<seoul.length; x++) {
+			if(seoul[x].charAt(0) == 'K') {
+				break;
+			}
+		}
+		return "김서방은 "+ x +"에 있다";
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		FindKim kim = new FindKim();
+		String[] names = {"Queen", "Tod", "Kim"};
+		System.out.println(kim.findKim(names));
 	}
 
 }
