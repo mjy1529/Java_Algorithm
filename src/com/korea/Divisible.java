2c09d0c054f105d2ee6814141c1b5bce731d7820
문주영 <enqlenwndud95@gmail.com>
2017-10-01 21:03:04 +0900

    LV1

---
+++ src/com/korea/Divisible.java
@@ -0,0 +1,27 @@
+package com.korea;
+
+import java.util.Arrays;
+
+public class Divisible {
+	public int[] divisible(int[] array, int divisor) {
+		int num = 0;
+		int[] tmp = new int[array.length];
+		for(int i=0; i<array.length; i++) {
+			if(array[i] % divisor == 0) {
+				tmp[num] = array[i];
+				num++;
+			}
+		}
+		int[] ret = new int[num];
+		for(int j=0; j<ret.length; j++) {
+			ret[j] = tmp[j];
+		}
+		return ret;
+	}
+	
+	public static void main(String[] args) {
+		Divisible div = new Divisible();
+		int[] array = {5,9,7,10};
+		System.out.println(Arrays.toString(div.divisible(array, 5)));
+	}
+}
---
+++ src/com/korea/PrintTriangle.java
@@ -0,0 +1,19 @@
+package com.korea;
+
+public class PrintTriangle {
+	public String printTriangle(int num) {
+		String star = "";
+		for(int i = 1; i<=num; i++) {
+			for(int j=1; j<=i; j++) {
+				star += '*';
+			}
+			star += "\n";
+		}
+		return star;
+	}
+	
+	public static void main(String[] args) {
+		PrintTriangle pt = new PrintTriangle();
+		System.out.println(pt.printTriangle(3));
+	}
+}
