bf955960351fb8f18e3941663a990fffe85efefb
문주영 <enqlenwndud95@gmail.com>
2017-10-01 21:08:15 +0900

    행렬의 덧셈

--- src/com/korea/SumMatrix.java
+++ src/com/korea/SumMatrix.java
@@ -3,7 +3,6 @@ package com.korea;
 class SumMatrix {
 	int[][] sumMatrix(int[][] A, int[][] B) {
 		int[][] answer = {{0, 0}, {0, 0}};
-
 		for(int i=0; i<A.length; i++) {
 			for(int j=0; j<A[i].length; j++) {
 				answer[i][j] = A[i][j] + B[i][j];
