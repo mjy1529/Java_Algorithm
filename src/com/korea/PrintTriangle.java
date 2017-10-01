f80008fca7352e6afd72045813818273ae7d9d76
문주영 <enqlenwndud95@gmail.com>
2017-10-01 21:08:55 +0900

    삼각형 출력하기

--- src/com/korea/PrintTriangle.java
+++ src/com/korea/PrintTriangle.java
@@ -11,7 +11,6 @@ public class PrintTriangle {
 		}
 		return star;
 	}
-	
 	public static void main(String[] args) {
 		PrintTriangle pt = new PrintTriangle();
 		System.out.println(pt.printTriangle(3));
