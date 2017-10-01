dfac7b499b92446015dbd92c3397a4656cd376f3
문주영 <enqlenwndud95@gmail.com>
2017-10-01 21:10:53 +0900

    배열의 평균 구하기

--- src/com/korea/GetMean.java
+++ src/com/korea/GetMean.java
@@ -8,11 +8,10 @@ public class GetMean {
       }
       return sum / (array.length);
     }
-
+    
     public static void main(String[] args) {
         int x[] = {5, 4, 3};
         GetMean getMean = new GetMean();
-        // 아래는 테스트로 출력해 보기 위한 코드입니다.
         System.out.println("평균값 : " + getMean.getMean(x));
     }
 }
