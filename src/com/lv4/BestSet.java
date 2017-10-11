e9edf80b1842ada55a5d95c1062d069510e04481
문주영 <enqlenwndud95@gmail.com>
2017-10-11 21:31:08 +0900

    최고의 집합

--- src/com/lv4/BestSet.java
+++ src/com/lv4/BestSet.java
@@ -1,6 +1,8 @@
 //최고의 집합
 package com.lv4;
 
+import java.util.Arrays;
+
 public class BestSet {
 	public int[] bestSet(int n, int s) {
 		int[] answer = new int[n];
@@ -14,6 +16,6 @@ public class BestSet {
 	}
 	public static void main(String[] args) {
 		BestSet c = new BestSet();
-		System.out.println(c.bestSet(3, 13));
+		System.out.println(Arrays.toString(c.bestSet(3, 13)));
 	}
 }
